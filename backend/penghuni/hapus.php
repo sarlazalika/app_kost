b7f113598633985790ec9ff8d857c3ce34074297
sarlazalika <sarlazalika@gmail.com>
2025-07-14 11:16:40 +0700

    Tambah fitur Delete (hapus data) penghuni di backend/penghuni/hapus.php

---
+++ backend/penghuni/hapus.php
@@ -0,0 +1,12 @@
+<?php
+include '../db.php';
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+if ($id > 0) {
+    $query = "DELETE FROM tb_penghuni WHERE id = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 'i', $id);
+    mysqli_stmt_execute($stmt);
+}
+header('Location: index.php');
+exit; 
